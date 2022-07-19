133cb323b8035ad8bcf794a9819677d56900ae73
Alex <alexandru.solomon9999@gmail.com>
2022-07-19 22:48:23 +0300

    Initialised the main python script

---
+++ src/main.py
@@ -0,0 +1,23 @@
+from flask import Flask, redirect, request, render_template, url_for, session
+from flask_sqlalchemy import SQLAlchemy
+import time
+
+app = Flask(__name__)
+
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///users.sqlite3'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+db = SQLAlchemy(app)
+
+app.secret_key = "mysecret"
+parola_juriu = "ZWAKc9gyM8"
+admin_pass = "alex.balex25"
+
+
+@app.route("/", methods=['POST', 'GET'])
+def judge_public():
+    return render_template("index.html")
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
