0c539d22e8fc951315629e811675340e6c9f012c
Alex <alexandru.solomon9999@gmail.com>
2022-07-20 10:58:07 +0300

    Initialised the html script for deciding on whether to create a code or type an existing one

--- src/main.py
+++ src/main.py
@@ -1,5 +1,8 @@
 from flask import Flask, redirect, request, render_template, url_for, session
 from flask_sqlalchemy import SQLAlchemy
+import pyperclip
+import random
+import string
 import time
 
 app = Flask(__name__)
@@ -21,7 +24,26 @@ def judge_public():
 
 @app.route("/play_friend", methods=['POST', 'GET'])
 def play_friends_page():
-    return render_template("play_friend_main.html")
+    current_code = generate_random_code()
+    func_dict = {
+        'copy_func': copy_text_to_clipboard
+    }
+    return render_template("play_friend_main.html", current_code=current_code, func_dict=func_dict)
+
+
+@app.route("/create_put_code", methods=['POST', 'GET'])
+def create_or_put_code():
+    return render_template("create_or_put_code.html")
+
+
+def copy_text_to_clipboard(text_to_copy: str):
+    pyperclip.copy(text_to_copy)
+
+
+def generate_random_code(length=8):
+    # With combination of lower and upper case
+    result_str = ''.join(random.choice(string.ascii_letters) for i in range(length))
+    return result_str
 
 
 if __name__ == "__main__":
